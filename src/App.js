29b0ad519e0765be89f520eadcb2f98e7c590b7a
Linda Blom <linda.bostrom@tele2.com>
2022-02-14 15:10:23 +0100

    test

--- src/App.js
+++ src/App.js
@@ -9,14 +9,26 @@ function App() {
   const [game, setGames] = useState([]);
 
   return (
-    <div className="App">
+    <div className="App" style={{ 
+      backgroundImage: `url(${process.env.PUBLIC_URL + '/game.jpg'})`,
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      height: '100vh'
+    }}>
+      <h1>Vad ska vi spela idag?</h1>
       <div className="grid-container">
-        <GetRandom game={game} />
-        <GameForm game={game} setGames={setGames} />
+        <div className="grid-content">
+          <GetRandom game={game} />
+        </div>
+        <div className="grid-content">
+          <GameForm game={game} setGames={setGames} />
+        </div>
         <div className="three">
           <h4>vilka val har vi?</h4>
           <RemoveGame game={game} setGames={setGames} />
         </div>
+
       </div>
     </div>
   );
